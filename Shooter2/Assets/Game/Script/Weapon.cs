0e280d92c9826bbc2a7b3c81549ea2ff6e9555c0
Marcel van Duijn <epicmars264@gmail.com>
2018-03-05 12:09:50 +0100

    Commit

--- Shooter2/Assets/Game/Script/Weapon.cs
+++ Shooter2/Assets/Game/Script/Weapon.cs
@@ -1,48 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
-    public int gunId;
-    public GameObject flash;
-    public Animator flashAnim;
     ObjectPool_Script[] objectPoolScript;
-    public GameObject[] weapons;
-    public GameObject[] objectPool;
-    public Transform[] shootPoint;
-    public float[] bulletSpeed;
-    public Vector3[] normalPos;
-    public Vector3[] aimPos;
-    public Animator[] anim;
-    public int[] ammo, magazineSize;
+    [SerializeField]private int gunId;
+    [SerializeField]private GameObject flash;
+    [SerializeField]private Animator flashAnim;
+    [SerializeField]private GameObject[] weapons, objectPool;
+    [SerializeField]private Transform[] shootPoint;
+    [SerializeField]private float[] bulletSpeed, reloadTime;
+    [SerializeField]private Vector3[] normalPos, aimPos;
+    [SerializeField]private Animator[] anim;
+    [SerializeField]private int[] ammo, magazineSize;
+    [SerializeField]private Text[] textUI;
+
+    private float[] reloadTimeReset;
     private int[] currentAmmo;
+    private bool reloading;
 
     void Start () 
     {
         objectPoolScript = new ObjectPool_Script[weapons.Length];
         anim = new Animator[weapons.Length];
+        currentAmmo = new int[weapons.Length];
+        reloadTimeReset = new float[weapons.Length];
         flashAnim = flash.GetComponent<Animator>();
         for (int i = 0; i < objectPool.Length; i++)
         {
             objectPoolScript[i] = (ObjectPool_Script)objectPool[i].GetComponent(typeof(ObjectPool_Script));
             anim[i] = weapons[i].GetComponent<Animator>();
+            reloadTimeReset[i] = reloadTime[i];
         }
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && gunId <= weapons.Length)
+        if (Input.GetMouseButtonDown(0) && gunId <= weapons.Length && currentAmmo[gunId] > 0)
         {
             Fire(gunId);
             anim[gunId].SetTrigger("M4");
             flashAnim.SetTrigger("Flash");
             flash.transform.localPosition = shootPoint[gunId].transform.localPosition + weapons[gunId].transform.localPosition;
+            currentAmmo[gunId] -= 1;
         }
         if (Input.GetMouseButton(1) && gunId <= weapons.Length)
         {
             weapons[gunId].transform.localPosition = new Vector3(aimPos[gunId].x,aimPos[gunId].y,aimPos[gunId].z);
-            Debug.Log("aim");
         }
         if (Input.GetMouseButtonUp(1) && gunId <= weapons.Length)
         {
@@ -61,6 +67,19 @@ public class Weapon : MonoBehaviour {
         {
             gunId = 2;
         }
+        if(Input.GetKeyDown(KeyCode.R) && currentAmmo[gunId] != magazineSize[gunId])
+        {
+            reloading = true;
+        }
+        if(reloading)
+        {
+            reloadTime[gunId] -= 1 * Time.deltaTime;
+            if(reloadTime[gunId] <= 0)
+            {
+                Reload();
+                reloading = false;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -76,8 +95,23 @@ public class Weapon : MonoBehaviour {
                 weapons[i].SetActive(true);
             }
         }
+        SetUI();
     }
 
+    void Reload()
+    {
+        reloadTime[gunId] = reloadTimeReset[gunId];
+        if (currentAmmo[gunId] <= 0 && ammo[gunId] >= magazineSize[gunId])
+        {
+            currentAmmo[gunId] += magazineSize[gunId];
+            ammo[gunId] -= magazineSize[gunId];
+        }
+        if (currentAmmo[gunId] <= 0 && ammo[gunId] < magazineSize[gunId])
+        {
+            currentAmmo[gunId] += ammo[gunId];
+            ammo[gunId] = 0;
+        }
+    }
 
     void Fire(int gunID)
     {
@@ -92,5 +126,11 @@ public class Weapon : MonoBehaviour {
                 break;
             }
         }
+    }
+
+    void SetUI()
+    {
+        textUI[0].text = currentAmmo[gunId].ToString();
+        textUI[1].text = ammo[gunId].ToString();
     }
 }
---
+++ Shooter2/Assets/Game/Script/WeaponNew.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WeaponNew : MonoBehaviour {
+
+    [SerializeField]private int gunId;
+    [SerializeField]private Text[] textUI;
+    [SerializeField]private GameObject flash;
+    [SerializeField]private Animator flashAnim;
+
+    public Weapons[] weaponsClass;
+
+
+    void Start () 
+    {
+        flashAnim = flash.GetComponent<Animator>();
+        for (int i = 0; i < weaponsClass.Length; i++)
+        {
+            weaponsClass[gunId].objectPoolScript = (ObjectPool_Script)weaponsClass[gunId].objectPool.GetComponent(typeof(ObjectPool_Script));
+            weaponsClass[gunId].anim = weaponsClass[gunId].weapon.GetComponent<Animator>();
+            weaponsClass[gunId].reloadTimeReset = weaponsClass[gunId].reloadTime;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && gunId <= weaponsClass.Length && weaponsClass[gunId].currentAmmo > 0)
+        {
+            Fire(gunId);
+            weaponsClass[gunId].anim.SetTrigger("M4");
+            flashAnim.SetTrigger("Flash");
+            flash.transform.localPosition = weaponsClass[gunId].shootPoint.transform.localPosition + weaponsClass[gunId].weapon.transform.localPosition;
+            weaponsClass[gunId].currentAmmo -= 1;
+        }
+        if (Input.GetMouseButton(1) && gunId <= weaponsClass.Length)
+        {
+            weaponsClass[gunId].weapon.transform.localPosition = new Vector3(weaponsClass[gunId].aimPos.x, weaponsClass[gunId].aimPos.y, weaponsClass[gunId].aimPos.z);
+        }
+        if (Input.GetMouseButtonUp(1) && gunId <= weaponsClass.Length)
+        {
+            weaponsClass[gunId].weapon.transform.localPosition = new Vector3(weaponsClass[gunId].normalPos.x, weaponsClass[gunId].normalPos.y, weaponsClass[gunId].normalPos.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            gunId = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            gunId = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            gunId = 2;
+        }
+        if(Input.GetKeyDown(KeyCode.R) && weaponsClass[gunId].currentAmmo != weaponsClass[gunId].magazineSize)
+        {
+            weaponsClass[gunId].reloading = true;
+        }
+        if(weaponsClass[gunId].reloading)
+        {
+            weaponsClass[gunId].reloadTime -= 1 * Time.deltaTime;
+            if(weaponsClass[gunId].reloadTime <= 0)
+            {
+                Reload();
+                weaponsClass[gunId].reloading = false;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < weaponsClass.Length; i++)
+        {
+            if (weaponsClass[gunId].weapon.activeSelf && i != gunId)
+            {
+                weaponsClass[gunId].weapon.SetActive(false);
+            }
+            if (!weaponsClass[gunId].weapon.activeSelf && i == gunId)
+            {
+                weaponsClass[gunId].weapon.SetActive(true);
+            }
+        }
+        SetUI();
+    }
+
+    void Reload()
+    {
+        weaponsClass[gunId].reloadTime = weaponsClass[gunId].reloadTimeReset;
+        if (weaponsClass[gunId].currentAmmo <= 0 && weaponsClass[gunId].ammo >= weaponsClass[gunId].magazineSize)
+        {
+            weaponsClass[gunId].currentAmmo += weaponsClass[gunId].magazineSize;
+            weaponsClass[gunId].ammo -= weaponsClass[gunId].magazineSize;
+        }
+        if (weaponsClass[gunId].currentAmmo <= 0 && weaponsClass[gunId].ammo < weaponsClass[gunId].magazineSize)
+        {
+            weaponsClass[gunId].currentAmmo += weaponsClass[gunId].ammo;
+            weaponsClass[gunId].ammo = 0;
+        }
+    }
+
+    void Fire(int gunID)
+    {
+        for (int i = 0; i < weaponsClass[gunId].objectPoolScript.objects.Count; i++)
+        {
+            if (!weaponsClass[gunId].objectPoolScript.objects[i].activeInHierarchy)
+            {
+                weaponsClass[gunId].objectPoolScript.objects[i].transform.position = weaponsClass[gunId].shootPoint.transform.position;
+                weaponsClass[gunId].objectPoolScript.objects[i].transform.rotation = weaponsClass[gunId].shootPoint.transform.rotation;
+                weaponsClass[gunId].objectPoolScript.objects[i].SetActive(true);
+                weaponsClass[gunId].objectPoolScript.objects[i].GetComponent<Rigidbody>().velocity = weaponsClass[gunId].objectPoolScript.objects[i].transform.forward * weaponsClass[gunId].bulletSpeed;
+                break;
+            }
+        }
+    }
+
+    void SetUI()
+    {
+        textUI[0].text = weaponsClass[gunId].currentAmmo.ToString();
+        textUI[1].text = weaponsClass[gunId].ammo.ToString();
+    }
+}
+
+[System.Serializable]
+public class Weapons
+{
+    public GameObject weapon, objectPool;
+    public Transform shootPoint;
+    public float bulletSpeed, reloadTime;
+    public Vector3 normalPos, aimPos;
+    public int ammo, magazineSize;
+
+    [HideInInspector]public Animator anim;
+    [HideInInspector]public ObjectPool_Script objectPoolScript;
+    [HideInInspector]public float reloadTimeReset;
+    [HideInInspector]public int currentAmmo;
+    [HideInInspector]public bool reloading;
+}
