10514dabb335c8b40151b3f82e791ab48c432165
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2018-03-24 20:57:48 +0100

    Commit

---
+++ Shooter2/Assets/Game/Script/HashTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashTest : MonoBehaviour 
+{
+    [Header("Input")]
+    public string input;
+    public string inputHash,hash,decryptHash;
+    public bool boolInput, boolInputHash, boolHash, boolDecryptHash;
+
+    [Header("Output")]
+    public int hashCount;
+
+	void Start () 
+    {
+        if (boolInput){Input();}
+        if (boolInput){InputHash();}
+        if (boolInput){Hash();}
+        if (boolInput){DecryptHash();}
+	}
+    void Input () 
+    {
+
+	}
+    void InputHash()
+    {
+        hashCount = inputHash.Length;
+    }
+    void Hash()
+    {
+
+    }
+    void DecryptHash()
+    {
+
+    }
+}
