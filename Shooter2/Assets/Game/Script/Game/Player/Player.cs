174bb9c7a3dfe70b32e7cba8aa98d9103d864261
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2018-03-28 19:50:17 +0200

    Commit

---
+++ Shooter2/Assets/Game/Script/Game/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+    [SerializeField]private float health,armor,maxHealth,maxArmor;
+
+    [SerializeField]private Pickups[] pickupsScript;
+
+	void Update () 
+    {
+        if (health >= maxHealth)
+        { health = maxHealth;}
+        if (armor >= maxArmor)
+        { armor = maxArmor;}
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < pickupsScript.Length; i++)
+        {
+            if (other.gameObject == pickupsScript[i].pickupObject)
+            {
+                if (health <= maxHealth && pickupsScript[i].health)
+                {
+                    health += pickupsScript[i].getAmount;
+                    other.gameObject.SetActive(false);
+                }
+                if (armor <= maxArmor && pickupsScript[i].armor)
+                {
+                    health += pickupsScript[i].getAmount;
+                    other.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
+
+
+[System.Serializable]
+public struct Pickups
+{
+    public GameObject pickupObject;
+    public float getAmount;
+    public bool health,armor;
+}
