aad9ee53b61ce85df31571be4a17253221d95437
Marcel van Duijn <epicmars264@gmail.com>
2018-04-11 09:41:05 +0200

    Commit

---
+++ Shooter2/Assets/Game/Script/Game/Other/TestScene/TestScene_GotoScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TestScene_GotoScene : MonoBehaviour {
+
+    [SerializeField]private int sceneIndex;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
